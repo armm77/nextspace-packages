dfbf6fdac1e384b21872c9d441116378a40e2deb
armm77 <armm77@icloud.com>
2025-07-13 16:15:58 -0400

    The operating system was changed to Fedora Linux 42.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # nextspace-packages
-[NEXTSPACE](https://github.com/trunkmaster/nextspace) Weekly Packs for Rocky Linux and AlmaLinux 8. 
+[NEXTSPACE](https://github.com/trunkmaster/nextspace) Weekly Packs for Fedora 42. 
--- reinstall-nextspace.sh
+++ reinstall-nextspace.sh
@@ -3,20 +3,17 @@
 # This script should be placed along with NSUser and NSDeveloper
 # directories.
 
-dnf -y reinstall https://github.com/armm77/nextspace-packages/raw/main/nextspace/NSUser/libdispatch-5.4.2-1.el8.x86_64.rpm
-dnf -y reinstall https://github.com/armm77/nextspace-packages/raw/main/nextspace/NSDeveloper/libdispatch-devel-5.4.2-1.el8.x86_64.rpm
-dnf -y reinstall https://github.com/armm77/nextspace-packages/raw/main/nextspace/NSUser/libcorefoundation-5.4.2-0.el8.x86_64.rpm
-dnf -y reinstall https://github.com/armm77/nextspace-packages/raw/main/nextspace/NSDeveloper/libcorefoundation-devel-5.4.2-0.el8.x86_64.rpm
-dnf -y reinstall https://github.com/armm77/nextspace-packages/raw/main/nextspace/NSUser/libobjc2-2.1-0.el8.x86_64.rpm
-dnf -y reinstall https://github.com/armm77/nextspace-packages/raw/main/nextspace/NSDeveloper/libobjc2-devel-2.1-0.el8.x86_64.rpm
-dnf -y reinstall https://github.com/armm77/nextspace-packages/raw/main/nextspace/NSUser/nextspace-core-0.95-12.el8.x86_64.rpm
-dnf -y reinstall https://github.com/armm77/nextspace-packages/raw/main/nextspace/NSDeveloper/nextspace-core-devel-0.95-12.el8.x86_64.rpm
-dnf -y reinstall https://github.com/armm77/nextspace-packages/raw/main/nextspace/NSUser/libwraster-5.0.0-0.el8.x86_64.rpm
-dnf -y reinstall https://github.com/armm77/nextspace-packages/raw/main/nextspace/NSDeveloper/libwraster-devel-5.0.0-0.el8.x86_64.rpm
-dnf -y reinstall https://github.com/armm77/nextspace-packages/raw/main/nextspace/NSUser/nextspace-gnustep-1_28_0_nextspace-1.el8.x86_64.rpm
-dnf -y reinstall https://github.com/armm77/nextspace-packages/raw/main/nextspace/NSDeveloper/nextspace-gnustep-devel-1_28_0_nextspace-1.el8.x86_64.rpm
-dnf -y reinstall https://github.com/armm77/nextspace-packages/raw/main/nextspace/NSUser/nextspace-frameworks-0.91-0.el8.x86_64.rpm
-dnf -y reinstall https://github.com/armm77/nextspace-packages/raw/main/nextspace/NSDeveloper/nextspace-frameworks-devel-0.91-0.el8.x86_64.rpm
-dnf -y reinstall https://github.com/armm77/nextspace-packages/raw/main/nextspace/NSUser/nextspace-applications-0.91-0.el8.x86_64.rpm
-dnf -y reinstall https://github.com/armm77/nextspace-packages/raw/main/nextspace/NSDeveloper/nextspace-applications-devel-0.91-0.el8.x86_64.rpm
-#dnf -y reinstall https://github.com/armm77/nextspace-packages/raw/main/nextspace/NSUser/nextspace-selinux-0.91-0.el8.x86_64.rpm
+dnf -y reinstall https://github.com/armm77/nextspace-packages/raw/main/nextspace/NSUser/libcorefoundation-5.9.2-1.fc42.x86_64.rpm
+dnf -y reinstall https://github.com/armm77/nextspace-packages/raw/main/nextspace/NSDeveloper/libcorefoundation-devel-5.9.2-1.fc42.x86_64.rpm
+dnf -y reinstall https://github.com/armm77/nextspace-packages/raw/main/nextspace/NSUser/libobjc2-2.2.1-0.fc42.x86_64.rpm
+dnf -y reinstall https://github.com/armm77/nextspace-packages/raw/main/nextspace/NSDeveloper/libobjc2-devel-2.2.1-0.fc42.x86_64.rpm
+dnf -y reinstall https://github.com/armm77/nextspace-packages/raw/main/nextspace/NSUser/nextspace-core-0.95-12.fc42.x86_64.rpm
+dnf -y reinstall https://github.com/armm77/nextspace-packages/raw/main/nextspace/NSDeveloper/nextspace-core-devel-0.95-12.fc42.x86_64.rpm
+dnf -y reinstall https://github.com/armm77/nextspace-packages/raw/main/nextspace/NSUser/libwraster-7.0.1-0.fc42.x86_64.rpm
+dnf -y reinstall https://github.com/armm77/nextspace-packages/raw/main/nextspace/NSDeveloper/libwraster-devel-7.0.1-0.fc42.x86_64.rpm
+dnf -y reinstall https://github.com/armm77/nextspace-packages/raw/main/nextspace/NSUser/nextspace-gnustep-1.31.1_0.32.0-1.fc42.x86_64.rpm
+dnf -y reinstall https://github.com/armm77/nextspace-packages/raw/main/nextspace/NSDeveloper/nextspace-gnustep-devel-1.31.1_0.32.0-1.fc42.x86_64.rpm
+dnf -y reinstall https://github.com/armm77/nextspace-packages/raw/main/nextspace/NSUser/nextspace-frameworks-0.95-0.fc42.x86_64.rpm
+dnf -y reinstall https://github.com/armm77/nextspace-packages/raw/main/nextspace/NSDeveloper/nextspace-frameworks-devel-0.95-0.fc42.x86_64.rpm
+dnf -y reinstall https://github.com/armm77/nextspace-packages/raw/main/nextspace/NSUser/nextspace-applications-0.95-0.fc42.x86_64.rpm
+dnf -y reinstall https://github.com/armm77/nextspace-packages/raw/main/nextspace/NSDeveloper/nextspace-applications-devel-0.95-0.fc42.x86_64.rpm
